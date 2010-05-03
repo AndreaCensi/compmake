e48065cf16a28b9b081068ac55ede28d6ae73f61
Andrea Censi <github@censi.org>
2010-05-03 17:31:02 +0000

    Fixed bug with pose sampling

--- compmake/utils/capture.py
+++ compmake/utils/capture.py
@@ -5,16 +5,24 @@ from StringIO import StringIO
 
 class StreamCapture:
     def __init__(self, transform=None, dest=None):
+        ''' dest has write() and flush() '''
         self.buffer = StringIO()
         self.dest = dest
         self.transform = transform
         
     def write(self, s):
         self.buffer.write(s)
-        if self.transform:
-            s = self.transform(s)
         if self.dest:
-            self.dest.write(s)
+            lines = s.split('\n')
+            for line in lines:
+                # XXX I'm not sure why we get empty lines here
+                if line:
+                    if self.transform:
+                        line = self.transform(line)
+                    self.dest.write(line)
+                    self.dest.write('\n')
+                
+            self.dest.flush()
         
 
 class OutputCapture:
@@ -22,7 +30,7 @@ class OutputCapture:
     def __init__(self, prefix, echo=True):
         self.old_stdout = sys.stdout
         self.old_stderr = sys.stderr
-        t = lambda s: '%s| %s' % (prefix, colored(s, attrs=['dark']))
+        t = lambda s: '%s| %s' % (prefix, colored(s, 'cyan', attrs=['dark']))
         dest = {True: sys.stdout, False: None}[echo]     
         self.stdout_replacement = StreamCapture(transform=t, dest=dest)
         sys.stdout = self.stdout_replacement
