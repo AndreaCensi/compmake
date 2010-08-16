bcc53d1732ca0b9aed16dda0cb37e1ef66603be8
Andrea Censi <andrea@cds.caltech.edu>
2010-08-15 17:25:42 -0700

    Added a long-needed argument checking to the progress() function.

--- compmake/jobs/progress.py
+++ compmake/jobs/progress.py
@@ -9,12 +9,53 @@ def init_progress_tracking(my_callback):
     stack = []
     callback = my_callback 
     
-def progress(name, iterations, iteration_desc=None):
+def progress(taskname, iterations, iteration_desc=None):
+    '''Function used by the user to describe the state of the computation.
+    
+       Arguments:
+    
+       - name: must be a string, describing the current task.
+       
+       - iterations: must be a tuple of two integers (k,N), meaning
+        that the current iteration is the k-th out of N.
+        
+       - iteration_desc: an optional string describing the current iteration.
+       
+       Example::
+    
+            for i in range(n):
+                progress('Reading files', (i,n), 'processing file %s' % file[i])
+    '''
+    
+    if not isinstance(taskname, str):
+        raise ValueError('The first argument to progress() is the task name ' + 
+                         'and must be a string; you passed a %s.' % 
+                         taskname.__class__.__name__)
+    if not isinstance(iterations, tuple):
+        raise ValueError('The second argument to progress() must be a tuple,' + 
+                         ' you passed a %s.' % iterations.__class__.__name__)
+    if not len(iterations) == 2:
+        raise ValueError('The second argument to progress() must be a tuple ' + 
+                         ' of length 2, not of length %s.' % len(iterations))
+    
+    if not isinstance(iterations[0], int):
+        raise ValueError('The first element of the tuple passed to progress ' + 
+                         'must be  an integer, not a %s.' % 
+                         iterations[0].__class__.__name__)         
+    
+    if not iterations[1] is None and not isinstance(iterations[1], int):
+        raise ValueError('The second element of the tuple passed to progress ' + 
+                         'must be either None or an integer, not a %s.' % 
+                         iterations[1].__class__.__name__)
+       
+    if iterations[1] < iterations[0]:
+        raise ValueError('Invalid iteration tuple: %s' % str(iterations))
+       
     global stack
 
     found = False
     for i, stage in enumerate(stack):
-        if stage.name == name:
+        if stage.name == taskname:
             stack[i + 1:] = []
             stage.iterations = iterations
             stage.iteration_desc = iteration_desc
@@ -24,6 +65,6 @@ def progress(name, iterations, iteration_desc=None):
             callback(stack)
             break
     if not found:
-        stack.append(ProgressStage(name, iterations, iteration_desc))
+        stack.append(ProgressStage(taskname, iterations, iteration_desc))
         global callback
         callback(stack)
