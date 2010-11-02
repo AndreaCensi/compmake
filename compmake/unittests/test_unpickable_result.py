79c21532dfbec4b445c6eb70739a39d9dc56d479
Andrea Censi <andrea@cds.caltech.edu>
2010-11-02 14:41:54 -0700

    whitespace

--- compmake/unittests/test_unpickable_result.py
+++ compmake/unittests/test_unpickable_result.py
@@ -18,15 +18,11 @@ class TestUnpickable(unittest.TestCase):
         for key in storage.db.keys('*'):
             storage.db.delete(key)
     
-    
     def test_unpickable(self):
-        
         self.assertRaises(SerializationError, self.add_and_execute, f1)
         
-
     def add_and_execute(self, function):
         from compmake import comp, batch_command
         comp(function)
-        
         batch_command('clean')
         batch_command('make')
