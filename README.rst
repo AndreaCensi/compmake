020ae61212f00c26b579a20fc85045ca2bf803e2
Andrea Censi <andrea@cds.caltech.edu>
2010-12-20 10:56:47 -0800

    better setup.py and docs

---
+++ README.rst
@@ -0,0 +1,16 @@
+**Compmake** is a non-obtrusive module that provides:
+
+* ``make``--like facilities to your Python computations
+  (``make``, ``clean``, etc.), including caching of temporary
+  results (that is, you can interrupt your program, and
+  restart it without losing (much) data.) 
+
+* A console for inspecting failures and partial completion. 
+
+* Single-host (using the ``multiprocessing`` module) and
+  multiple-host parallelization (using ssh-spawned slaves). 
+  
+* Peace of mind!
+
+See <http://compmake.org> for documentation.
+
--- setup.py
+++ setup.py
@@ -1,23 +1,50 @@
+import os
 from setuptools import setup, find_packages
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='compmake',
     author="Andrea Censi",
     author_email="andrea@cds.caltech.edu",
     url='http://compmake.org',
+
+    description = 
+        "Compmake is a non-obtrusive module that provides "
+        "'make'-like facilities to your Python computations,"
+        "including caching of results, robustness to exceptions, "
+        "and multiprocessing/multihost parallel processing. ",
+
+    long_description = read('README.rst'),
+    keywords = "parallel processing, make, cmake, manager, recovery",
+    license = "LGPL",
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Hardware :: Symmetric Multi-processing',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',        
+    ],
+      
     version="0.9.5",
     packages=find_packages(),
     entry_points={
      'console_scripts': [
        'compmake = compmake.scripts.master:main'
-       ]
-       },
+      ]
+    },
     install_requires=['termcolor', 'setproctitle', 'readline'],
-    extras_require={
-    'multiprocessing':  ['redis']
-    # TODO: learn how to use this feature
-    # TODO: add gvgen
-    }
+    
+    tests_require=['nose']
+    # extras_require={
+    # 'multiprocessing':  ['redis']
+    # # TODO: learn how to use this feature
+    # # TODO: add gvgen
+    # }
 )
 
