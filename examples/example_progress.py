f4a2c1e2f7f98f818e086c2d74be516df0adb843
Andrea Censi <andrea@cds.caltech.edu>
2011-12-01 11:54:58 -0800

    minor

--- examples/example_progress.py
+++ examples/example_progress.py
@@ -5,23 +5,23 @@ def mylongfunction():
     
     directories = ['a', 'b', 'c', 'd', 'e']
     n = len(directories)
-    for i, dir in enumerate(directories):
-        progress('Processing directories (first)', (i, n), 'Directory %s' % dir)
+    for i, dirname in enumerate(directories):
+        progress('Processing directories (first)', (i, n), 'Directory %s' % dirname)
         
         N = 3
         for k in range(N):
             progress('Processing files (a)', (k, N), 'file #%d' % k)
             
-            time.sleep( 1)
+            time.sleep(1)
             
-    for i, dir in enumerate(directories):
-        progress('Processing directories (second)', (i, n), 'Directory %s' % dir)
+    for i, dirname in enumerate(directories):
+        progress('Processing directories (second)', (i, n), 'Directory %s' % dirname)
     
         N = 3
         for k in range(N):
             progress('Processing files (b)', (k, N), 'file #%d' % k)
             
-            time.sleep(  1)
+            time.sleep(1)
             
             
 comp(mylongfunction)
