73fb2ec02a6158e3e53925f9751eb562c07ef52b
Andrea Censi <andrea@cds.caltech.edu>
2013-05-20 17:53:15 -0700

    are assertions normal exceptions?

---
+++ src/compmake/unittests/test_assertions.py
@@ -0,0 +1,21 @@
+from . import  compmake_environment
+from ..structures import Cache
+from ..jobs import get_job_cache
+
+
+def job_success(*args, **kwargs):
+    pass
+
+
+def job_failure(*args, **kwargs):
+    assert False
+
+
+@compmake_environment
+def test_order():
+    from compmake import comp, batch_command
+    # make A -> B(fail) -> C
+    for i in range(10):
+        comp(job_failure, job_id='F%d' % i)
+    batch_command('parmake n=2')
+
