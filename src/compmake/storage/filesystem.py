1d2c29971d161a0c11ab40f074f4d561aa3e7a1b
Andrea Censi <andrea@cds.caltech.edu>
2013-06-12 16:41:21 -0700

    misc

--- src/compmake/storage/filesystem.py
+++ src/compmake/storage/filesystem.py
@@ -1,4 +1,4 @@
-from ..structures import CompmakeException, SerializationError
+from compmake.structures import CompmakeException, SerializationError
 from compmake.utils import find_pickling_error, safe_pickle_load, safe_pickle_dump
 from compmake import logger
 from glob import glob
