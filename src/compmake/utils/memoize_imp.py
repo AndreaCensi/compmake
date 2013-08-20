22b4ce5242d29bee1c93236fad290479b7e70f3a
Andrea Censi <andrea@cds.caltech.edu>
2013-08-20 16:03:10 +0200

    renaming

---
+++ src/compmake/utils/memoize_imp.py
@@ -0,0 +1,16 @@
+from . import frozendict2
+from decorator import decorator
+
+__all__ = ['memoize_simple']
+
+def memoize_simple(obj):
+    cache = obj.cache = {}
+
+    def memoizer(f, *args, **kwargs):
+        key = (args, frozendict2(kwargs))
+        if key not in cache:
+            cache[key] = f(*args, **kwargs)
+            # print('memoize: %s %d storage' % (obj, len(cache)))
+        return cache[key]
+    
+    return decorator(memoizer, obj)
