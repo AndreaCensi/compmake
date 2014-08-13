6bf313fb7cec0d04299c35ee7520ea5ce9a454c0
Andrea Censi <andrea@cds.caltech.edu>
2014-08-13 17:04:13 -0400

    fixed no import

--- src/compmake/ui/commands.py
+++ src/compmake/ui/commands.py
@@ -14,7 +14,7 @@ from .helpers import (GENERAL, ACTIONS, COMMANDS_ADVANCED,
 from .. import CompmakeConstants, get_compmake_status, get_compmake_config
 from ..context import Context
 from ..events import publish
-from ..jobs import (all_jobs, ClusterManager, ManagerLocal,
+from ..jobs import (all_jobs, ManagerLocal,
     MultiprocessingManager, clean_target, mark_remake, top_targets,
     parse_yaml_configuration, SGEManager)
 from ..structures import UserError, JobFailed, ShellExitRequested
@@ -22,7 +22,7 @@ from ..ui import info
 from .helpers import ui_command
 from ..jobs import PmakeManager
 from compmake.utils.safe_pickle import safe_pickle_dump
-from compmake.structures import CommandFailed, MakeFailed
+from compmake.structures import  MakeFailed
 
 
 ui_section(GENERAL)
@@ -217,28 +217,28 @@ def sgemake(job_list, context, cq):
     manager.add_targets(job_list)
     manager.process()
     return _raise_if_failed(manager)
-
-@ui_command(section=COMMANDS_CLUSTER, dbchange=True)
-def clustmake(job_list, context, cq):
-    ''' (experimental) Cluster equivalent of "make". '''
-    # job_list = list(job_list) # don't ask me why XXX
-    job_list = [x for x in job_list]
-
-    if not job_list:
-        db = context.get_compmake_db()
-        job_list = list(top_targets(db))
-
-    cluster_conf = get_compmake_config('cluster_conf')
-
-    if not os.path.exists(cluster_conf):
-        msg = ('Configuration file %r does not exist.' % cluster_conf)
-        raise UserError(msg)
-
-    hosts = parse_yaml_configuration(open(cluster_conf))
-    manager = ClusterManager(hosts=hosts, context=context, cq=cq)
-    manager.add_targets(job_list)
-    manager.process()
-    return _raise_if_failed(manager)
+# 
+# @ui_command(section=COMMANDS_CLUSTER, dbchange=True)
+# def clustmake(job_list, context, cq):
+#     ''' (experimental) Cluster equivalent of "make". '''
+#     # job_list = list(job_list) # don't ask me why XXX
+#     job_list = [x for x in job_list]
+# 
+#     if not job_list:
+#         db = context.get_compmake_db()
+#         job_list = list(top_targets(db))
+# 
+#     cluster_conf = get_compmake_config('cluster_conf')
+# 
+#     if not os.path.exists(cluster_conf):
+#         msg = ('Configuration file %r does not exist.' % cluster_conf)
+#         raise UserError(msg)
+# 
+#     hosts = parse_yaml_configuration(open(cluster_conf))
+#     manager = ClusterManager(hosts=hosts, context=context, cq=cq)
+#     manager.add_targets(job_list)
+#     manager.process()
+#     return _raise_if_failed(manager)
 
 
 @ui_command(section=ACTIONS, dbchange=True)
