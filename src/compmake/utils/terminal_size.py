9d4348200b4f94cd41402db7eb80302ee50862ba
Andrea Censi <censi@mit.edu>
2014-10-07 11:47:27 -0400

    fix

--- src/compmake/utils/terminal_size.py
+++ src/compmake/utils/terminal_size.py
@@ -6,26 +6,19 @@ __all__ = [
 
 def get_screen_columns():
     max_x, _ = getTerminalSize()  # @UnusedVariable
+
+    if max_x < 0 or max_x > 1024:
+        msg = 'Very weird max screen size: %d' % max_x
+        raise ValueError(msg)
+
     return max_x
 
 
 def getTerminalSize():
-    '''
+    """
     max_x, max_y = getTerminalSize()
-    '''
+    """
     import os
-
-    def ioctl_GWINSZ(fd):
-        try:
-            import fcntl
-            import termios
-            import struct
-
-            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
-                                                 '1234'))
-        except:
-            return None
-        return cr
 
     cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
     if not cr:
@@ -42,3 +35,15 @@ def getTerminalSize():
         except:
             cr = (25, 80)
     return int(cr[1]), int(cr[0])
+
+
+def ioctl_GWINSZ(fd):
+    try:
+        import fcntl
+        import termios
+        import struct
+
+        s = fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234')
+        return struct.unpack('hh', s)
+    except:
+        return None
