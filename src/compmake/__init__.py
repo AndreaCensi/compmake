c8866af069d0cc03767f7ff95ec3fe1a0ca8f6b3
Andrea Censi <acensi@ethz.ch>
2021-01-04 18:53:55 +0000

    Bump to version 7.1.2101041853

--- src/compmake/__init__.py
+++ src/compmake/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "7.1.2101041552"
-__date__ = "2021-01-04T15:52:32.794920"
+__version__ = "7.1.2101041853"
+__date__ = "2021-01-04T18:53:50.061351"
 
 from zuper_commons.logs import ZLogger
 
