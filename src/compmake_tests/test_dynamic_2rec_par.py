f968bca86b73804f9b68e05deb3181c898cb3784
Andrea Censi <acensi@ethz.ch>
2021-04-24 20:28:12 +0200

    misc

--- src/compmake_tests/test_dynamic_2rec_par.py
+++ src/compmake_tests/test_dynamic_2rec_par.py
@@ -1,3 +1,6 @@
+from unittest import SkipTest
+
+from zuper_typing import PYTHON_37
 from .mockup import mockup_recursive_5
 
 from .utils import Env, run_with_env
@@ -5,6 +8,8 @@ from .utils import Env, run_with_env
 
 @run_with_env
 async def test_dynamic2_recpar(env: Env):
+    if PYTHON_37:
+        raise SkipTest("Known weirdness in 3.7 multiprocessing")
     mockup_recursive_5(env.cc)
     await env.assert_cmd_success("parmake recurse=1;ls")
     await env.assert_jobs_equal("all", ["r1", "r2", "r3", "r4", "r5"])
