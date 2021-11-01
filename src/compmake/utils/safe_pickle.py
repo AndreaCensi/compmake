80be243b633e04dfbcaa530754e254924f269472
Andrea Censi <acensi@ethz.ch>
2021-11-01 10:54:24 +0100

    fix up

--- src/compmake/utils/safe_pickle.py
+++ src/compmake/utils/safe_pickle.py
@@ -6,26 +6,7 @@ __all__ = [
     "safe_pickle_dump",
     "safe_pickle_load",
 ]
-from zuper_commons.fs import safe_write, find_pickling_error, safe_read
-from zuper_commons.types import describe_type
 
+from zuper_commons.fs import safe_pickle_dump, safe_pickle_load
 
-def safe_pickle_dump(value, filename, protocol=pickle.HIGHEST_PROTOCOL, **safe_write_options):
-    with safe_write(filename, **safe_write_options) as f:
-        try:
-            pickle.dump(value, f, protocol)
-        except KeyboardInterrupt:
-            raise
-        except Exception:
-            msg = "Cannot pickle object of class %s" % describe_type(value)
-            logger.error(msg)
-            msg = find_pickling_error(value, protocol)
-            logger.error(msg)
-            raise
-
-
-def safe_pickle_load(filename):
-    # TODO: add debug check
-    with safe_read(filename) as f:
-        return pickle.load(f)
-        # TODO: add pickling debug
+_ = safe_pickle_dump, safe_pickle_load
