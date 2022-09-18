0e1eda4181dbe6f3dc9fdc1bb31a7f67fe0e639d
Andrea Censi <acensi@ethz.ch>
2022-09-18 15:21:23 +0200

    DEV-119 Do direct REST API commits on Github [ci skip]

--- src/compmake_utils/coloredterm.py
+++ src/compmake_utils/coloredterm.py
@@ -2,39 +2,16 @@ __all__ = [
     "termcolor_colored",
 ]
 
-# Available text colors:
-#         red, green, yellow, blue, magenta, cyan, white.
-#
-#     Available text highlights:
-#         on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white.
-#
-#     Available attributes:
-#         bold, dark, underline, blink, reverse, concealed.
-#
-#
-from typing import List, Optional
+from typing import Optional, Sequence
 
-from termcolor import colored as t_colored  # @UnresolvedImport
-
-from zuper_commons.text import joinlines
-from zuper_commons.types import check_isinstance
+from zuper_commons.ui import get_colorize_function
 
 
-def termcolor_colored(
-    s: str, color: Optional[str] = None, on_color: Optional[str] = None, attrs: Optional[List[str]] = None
-) -> str:
-    check_isinstance(s, str)
-    return joinlines(t_colored(x, color, on_color, attrs) for x in s.splitlines())
-
-
-#
-# try:
-#
-#
-# except:
-#     # TODO: logger
-#     sys.stderr.write('compmake can make use of the package "termcolor".' " Please install it.\n")
-#
-#     def termcolor_colored(x, color=None, on_color=None, attrs=None):
-#         """ emulation of the termcolor interface """
-#         return x
+def termcolor_colored(x, color: str, on_color: Optional[str] = None, attrs: Optional[Sequence[str]] = None):
+    cf = get_colorize_function(color, on_color, attrs)
+    colorize = True
+    # TODO: no colorize during tests
+    if colorize:  # @UndefinedVariable
+        return cf(x)
+    else:
+        return x
