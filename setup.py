da1bd8fb7d4a1271c29d6671e292a1bfad842b18
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-09-01 10:15:29 +0200

    [ci skip]

---
+++ setup.py
@@ -0,0 +1,29 @@
+import json
+
+from setuptools import setup
+
+with open("setup.json") as f:
+    data = json.load(f)
+
+# setup package
+params = dict(
+    name=data["package_name"],
+    author=data["author"],
+    author_email=data["author_email"],
+    url=data["url"],
+    tests_require=data["tests_require"],
+    extras_require={"test": data["tests_require"]},
+    install_requires=data["install_requires"],
+    package_dir={"": data["srcdir"]},
+    packages=data["packages"],
+    # py_modules=data["py_modules"],
+    long_description="",
+    version=data["version"],
+    entry_points=data["entry_points"],
+    zip_safe=data["zip_safe"],
+    include_package_data=data["include_package_data"],
+)
+
+setup(**params)
+
+# sigil 8668ee96e452bf23787aa653addf1878
